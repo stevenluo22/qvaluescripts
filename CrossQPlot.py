93a52801ac4cfdb8e0f311e7200484b14ccd72fb
stevenluo22 <sl206@rice.edu>
2025-02-20 01:08:08 -0600

    implement automation CrossQPlot.py

--- CrossQPlot.py
+++ CrossQPlot.py
@@ -10,7 +10,7 @@ import csv
 
 def run(args):
     # Define the file name
-    file_name = "cross_q_val_table.csv"
+    args.inputCSV
 
     # Initialize the dictionary
     cross_q_val_table = {}
@@ -32,19 +32,19 @@ def run(args):
     import numpy as np
 
     # Convert dictionary to a 2D NumPy array
-    q_val_array = np.array([[cross_q_val_table[i][j] for j in range(1, 9)] for i in range(1, 9)])
+    #q_val_array = np.array([[cross_q_val_table[i][j] for j in range(1, 9)] for i in range(1, 9)])
 
     # Define custom colormap going from red to yellow to green
     colors = [(1, 0, 0), (1, 1, 0), (0, 1, 0)]  # Red to Yellow to Green
     cmap_name = 'red_yellow_green'
     cm = LinearSegmentedColormap.from_list(cmap_name, colors, N=256)
 
-    file_name = f"cross_q_val.jpg"
+    file_name = args.outputplot
     q_val_map = sns.clustermap(cross_q_val_table, cmap = cm)
 
     # Access the colorbar and set its label
     cbar = q_val_map.ax_heatmap.collections[0].colorbar
-    cbar.set_label('Mutual Q value', fontsize=10)
+    cbar.set_label('Mutual Q value', fontsize=args.fontSize)
 
     # Axes
     ax = q_val_map.ax_heatmap
@@ -61,7 +61,7 @@ def run(args):
 def main(args=None):
     parser = argparse.ArgumentParser(
         description="Draw plots from cross Q data csv")
-    parser.add_argument("-i", "--inputCSV", help="csv file with list of pdb files paths", type=str)
+    parser.add_argument("-i", "--inputCSV", help="csv file with list of pdb files paths", default="CrossQ.csv", type=str)
     parser.add_argument("-o", "--outputplot", help="Name of output plot", default="CrossQ.jpg", type=str)
     parser.add_argument("-f", "--fontSize", help="font size", default=10, type=float)
 
